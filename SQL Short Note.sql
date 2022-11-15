486ba45205e3a9e311c5207d6942a584cb20259f
Md. Al-Mahmud <48083880+ALMahmud00@users.noreply.github.com>
2022-11-15 13:21:07 +0600

    Update SQL Short Note.sql

--- SQL Short Note.sql
+++ SQL Short Note.sql
@@ -7,3 +7,14 @@ output inserted.your_table_primary_key into @insertedIds
 where table_field = 'field_value'
 
 select id from @insertedIds
+
+
+--===== BASE-64 Encode-Decode
+DECLARE @MyString varchar(250) = 'akijGlobe@6598'
+DECLARE @Encoded varchar(250) 
+DECLARE @Decoded varchar(250)  
+ 
+set @Encoded = (SELECT CAST(@MyString as varbinary(max)) FOR XML PATH(''), BINARY BASE64)    
+set @Decoded = (SELECT CAST( CAST(@Encoded as XML ).value('.','varbinary(max)') AS varchar(max))) 
+
+select @Encoded, @Decoded
